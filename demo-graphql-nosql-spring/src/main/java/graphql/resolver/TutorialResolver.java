629e2019bad7c91a9b846815a0a8d138d62cfc80
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-05-05 10:32:26 +0200

    Update TutorialResolver.java

--- demo-graphql-nosql-spring/src/main/java/graphql/resolver/TutorialResolver.java
+++ demo-graphql-nosql-spring/src/main/java/graphql/resolver/TutorialResolver.java
@@ -1,12 +1,18 @@
 package graphql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import graphql.Author;
 import graphql.Tutorial;
+import graphql.AuthorRepository;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 @Component
 public class TutorialResolver implements GraphQLResolver<Tutorial> {
   @Autowired
-  public TutorialResolver() {
+  private AuthorRepository authorRepository;
+  public TutorialResolver(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+  public Author getAuthor(Tutorial tutorial) {
+    return authorRepository.findById(tutorial.getAuthor().getId()).orElseThrow(null);
   }
 }
-
